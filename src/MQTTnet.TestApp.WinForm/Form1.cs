bacd02836dda305082aec74f75a0a4c37d291f6e
SeppPenner <HansM2013@gmx.de>
2023-10-14 19:56:54 +0200

    Removed invalid copyright year.

--- src/MQTTnet.TestApp.WinForm/Form1.cs
+++ src/MQTTnet.TestApp.WinForm/Form1.cs
@@ -1,6 +1,6 @@
 // --------------------------------------------------------------------------------------------------------------------
 // <copyright file="Form1.cs" company="Hämmer Electronics">
-//   Copyright (c) 2020 All rights reserved.
+//   Copyright (c) All rights reserved.
 // </copyright>
 // <summary>
 //   The main form.
